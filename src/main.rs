2de69d490c7bb27980f8b924bde8c8f59ea211ab
interesnij <ochkarik1983@mail.ru>
2023-09-19 21:10:00 +0300

    J

---
+++ src/main.rs
@@ -0,0 +1,48 @@
+use actix_web::{
+    App,
+    HttpServer,
+    web,
+};
+use actix_cors::Cors;
+
+mod views;
+mod routes;
+mod errors;
+
+use crate::views::not_found_page;
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    use crate::routes::routes;
+    use actix_files::Files;
+    use std::time::Duration;
+    use actix_cors::Cors;
+    use actix_extensible_rate_limit::{
+        backend::{
+            SimpleInputFunctionBuilder,
+            memory::InMemoryBackend,
+        },
+        RateLimiter,
+    };
+    let limit_backend = InMemoryBackend::builder().build();
+
+    HttpServer::new(move || { 
+        let _files = Files::new("/static", "static/").show_files_listing();
+        let limit_input = SimpleInputFunctionBuilder::new(Duration::from_secs(1), 5)
+            .real_ip_key() 
+            .build();
+        let limit_middleware = RateLimiter::builder(limit_backend.clone(), limit_input)
+            .add_headers()
+            .build();
+
+        App::new()  
+            .wrap(limit_middleware) 
+            .default_service(web::route().to(not_found_page))
+            .service(_files)
+            .configure(routes)
+    }) 
+    .bind("69.167.186.207:9999")?   // prod
+    //.bind("69.167.186.207:9998")? // dev
+    .run()
+    .await
+} 
---
+++ src/views/pages.rs
@@ -0,0 +1,177 @@
+use actix_web::{
+    HttpRequest,
+    HttpResponse,
+    Responder,
+    web,
+    error::InternalError,
+    http::StatusCode,
+};
+use sailfish::TemplateOnce;
+use serde::{Serialize, Deserialize};
+
+
+pub fn pages_urls(config: &mut web::ServiceConfig) {
+    config.route("/", web::get().to(index_page));
+    config.route("/about", web::get().to(about_page));
+    config.route("/faqs", web::get().to(faqs_page));
+    config.route("/faqs-for-attorneys", web::get().to(attorneys_faqs_page));
+    config.route("/for-lawyers", web::get().to(for_lawyers_page));
+    config.route("/for-paralegals", web::get().to(for_paralegals_page));
+    config.route("/terms-and-conditions", web::get().to(terms_page));
+    config.route("/privacy-policy", web::get().to(policy_page));
+}
+
+pub fn get_ajax(req: &HttpRequest) -> u8 {
+    #[derive(Debug, Deserialize)]
+    struct Params {
+        pub ajax: Option<u8>,
+    }
+    let params_some = web::Query::<Params>::from_query(&req.query_string());
+    let mut is_ajax = 0;
+
+    if params_some.is_ok() {
+        let params = params_some.unwrap();
+        if params.ajax.is_some() {
+            is_ajax = params.ajax.unwrap();
+        }
+        else {
+            is_ajax = 0;
+        }
+    }
+
+    is_ajax
+}
+
+pub async fn get_first_load_page (    
+    title:       String,
+    description: String,
+    uri:         String,
+) -> actix_web::Result<HttpResponse> {
+    #[derive(TemplateOnce)]
+    #[template(path = "first_load.stpl")]
+    struct Template {
+        title:       String,
+        description: String,
+        uri:         String,
+    }
+    let body = Template {
+        title:       title,
+        description: description,
+        uri:         uri,
+    }
+    .render_once()
+    .map_err(|e| InternalError::new(e, StatusCode::INTERNAL_SERVER_ERROR))?;
+    Ok(HttpResponse::Ok().content_type("text/html; charset=utf-8").body(body))
+}
+
+pub async fn index_page (
+    req: HttpRequest
+) -> actix_web::Result<HttpResponse> {
+    let is_ajax = get_ajax(&req); 
+    #[derive(TemplateOnce)]
+    #[template(path = "index.stpl")]
+    struct DesctopAuthTemplate {
+        title:       String,
+        description: String,
+        is_ajax:     u8,
+    }
+    let body = DesctopAuthTemplate {
+        title:       "JusMediation | How It Works".to_string(),
+        description: "JusMediation is an easy-to-use, app-enabled platform that allows you to find the right lawyer to meet your needs across a wide range of legal areas and industries from one-time consults to complex issues with ease and transparency from start to finish of your legal issues.".to_string(),
+        is_ajax:     is_ajax,
+    }
+    .render_once()
+    .map_err(|e| InternalError::new(e, StatusCode::INTERNAL_SERVER_ERROR))?;
+    Ok(HttpResponse::Ok().content_type("text/html; charset=utf-8").body(body))
+}
+
+pub async fn about_page (
+    req: HttpRequest
+) -> actix_web::Result<HttpResponse> {
+    let is_ajax = get_ajax(&req); 
+    #[derive(TemplateOnce)]
+    #[template(path = "about.stpl")]
+    struct DesctopAuthTemplate {
+        title:       String,
+        description: String,
+        is_ajax:     u8,
+    }
+    let body = DesctopAuthTemplate {
+        title:       "JusMediation | About".to_string(),
+        description: "JusMediation is an easy-to-use, app-enabled platform that allows you to find the right lawyer to meet your needs across a wide range of legal areas and industries from one-time consults to complex issues with ease and transparency from start to finish of your legal issues.".to_string(),
+        is_ajax:     is_ajax,
+    }
+    .render_once()
+    .map_err(|e| InternalError::new(e, StatusCode::INTERNAL_SERVER_ERROR))?;
+    Ok(HttpResponse::Ok().content_type("text/html; charset=utf-8").body(body))
+}
+
+pub async fn attorneys_faqs_page() -> actix_web::Result<HttpResponse> {
+    #[derive(TemplateOnce)]
+    #[template(path = "attorneys_faqs.stpl")]
+    struct DesctopAuthTemplate;
+    let body = DesctopAuthTemplate{}
+    .render_once()
+    .map_err(|e| InternalError::new(e, StatusCode::INTERNAL_SERVER_ERROR))?;
+    Ok(HttpResponse::Ok().content_type("text/html; charset=utf-8").body(body))
+}
+
+pub async fn faqs_page() -> actix_web::Result<HttpResponse> {
+    #[derive(TemplateOnce)]
+    #[template(path = "faqs.stpl")]
+    struct DesctopAuthTemplate;
+    let body = DesctopAuthTemplate{}
+    .render_once()
+    .map_err(|e| InternalError::new(e, StatusCode::INTERNAL_SERVER_ERROR))?;
+    Ok(HttpResponse::Ok().content_type("text/html; charset=utf-8").body(body))
+}
+
+pub async fn for_lawyers_page() -> actix_web::Result<HttpResponse> {
+    #[derive(TemplateOnce)]
+    #[template(path = "for_lawyers.stpl")]
+    struct DesctopAuthTemplate;
+    let body = DesctopAuthTemplate{}
+    .render_once()
+    .map_err(|e| InternalError::new(e, StatusCode::INTERNAL_SERVER_ERROR))?;
+    Ok(HttpResponse::Ok().content_type("text/html; charset=utf-8").body(body))
+}
+
+pub async fn for_paralegals_page() -> actix_web::Result<HttpResponse> {
+    #[derive(TemplateOnce)]
+    #[template(path = "for_paralegals.stpl")]
+    struct DesctopAuthTemplate;
+    let body = DesctopAuthTemplate{}
+    .render_once()
+    .map_err(|e| InternalError::new(e, StatusCode::INTERNAL_SERVER_ERROR))?;
+    Ok(HttpResponse::Ok().content_type("text/html; charset=utf-8").body(body))
+}
+
+pub async fn not_found_page() -> actix_web::Result<HttpResponse> {
+    #[derive(TemplateOnce)]
+    #[template(path = "not_found.stpl")]
+    struct DesctopAuthTemplate;
+    let body = DesctopAuthTemplate{}
+    .render_once()
+    .map_err(|e| InternalError::new(e, StatusCode::INTERNAL_SERVER_ERROR))?;
+    Ok(HttpResponse::Ok().content_type("text/html; charset=utf-8").body(body))
+}
+
+pub async fn terms_page() -> actix_web::Result<HttpResponse> {
+    #[derive(TemplateOnce)]
+    #[template(path = "terms.stpl")]
+    struct DesctopAuthTemplate;
+    let body = DesctopAuthTemplate{}
+    .render_once()
+    .map_err(|e| InternalError::new(e, StatusCode::INTERNAL_SERVER_ERROR))?;
+    Ok(HttpResponse::Ok().content_type("text/html; charset=utf-8").body(body))
+}
+
+pub async fn policy_page() -> actix_web::Result<HttpResponse> {
+    #[derive(TemplateOnce)]
+    #[template(path = "policy.stpl")]
+    struct DesctopAuthTemplate;
+    let body = DesctopAuthTemplate{}
+    .render_once()
+    .map_err(|e| InternalError::new(e, StatusCode::INTERNAL_SERVER_ERROR))?;
+    Ok(HttpResponse::Ok().content_type("text/html; charset=utf-8").body(body))
+}
