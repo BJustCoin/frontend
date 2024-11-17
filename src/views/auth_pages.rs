36d729e96c3033733b5398930acaa65891568f8b
interesnij <ochkarik1983@mail.ru>
2024-11-17 21:37:41 +0300

    J

--- src/views/auth_pages.rs
+++ src/views/auth_pages.rs
@@ -17,7 +17,9 @@ use crate::utils::{
 };
 use crate::utils::request_post;
 use actix_session::Session;
-
+use actix_multipart::{Field, Multipart};
+use std::borrow::BorrowMut;
+use futures::StreamExt;
 
 pub fn auth_urls(config: &mut web::ServiceConfig) {
     config.route("/login/", web::get().to(auth_page));
@@ -56,13 +58,36 @@ pub struct NewUser2 {
     pub password:   String,
 }
 
-pub async fn login(session: Session, data: LoginUser) -> actix_web::Result<HttpResponse> {
-    if is_signed_in(&session) {
+pub async fn login_form(payload: &mut Multipart) -> LoginUser {
+    let mut form: LoginUser = LoginUser {
+        email:    "".to_string(),
+        password: "".to_string(),
+    };
+
+    while let Some(item) = payload.next().await {
+        let mut field: Field = item.expect("split_payload err");
+        while let Some(chunk) = field.next().await {
+            let data = chunk.expect("split_payload err chunk");
+            if let Ok(s) = std::str::from_utf8(&data) {
+                let data_string = s.to_string();
+                if field.name() == "email" {
+                    form.email = data_string
+                } else if field.name() == "password" {
+                    form.password = data_string
+                }
+            }
+        }
+    }
+    form
+}
+pub async fn login(session: Session, mut payload: Multipart) -> actix_web::Result<HttpResponse> {
+    if is_signed_in(&session) { 
         return crate::views::not_found_page(session).await;
-    } 
+    }
+    let form = login_form(payload.borrow_mut()).await;
     let l_data = LoginUser {
-        email:    data.email.clone(),
-        password: data.password.clone(),
+        email:    form.email.clone(),
+        password: form.password.clone(),
     }; 
     let res = request_post::<LoginUser, AuthResp> (
         URL.to_owned() + &"/login/".to_string(),
@@ -78,19 +103,55 @@ pub async fn login(session: Session, data: LoginUser) -> actix_web::Result<HttpR
         Err(_) => crate::views::not_found_page(session).await,
     }
 }
-pub async fn signup(session: Session, data: Json<NewUser>) -> actix_web::Result<HttpResponse> {
+
+#[derive(Deserialize, Serialize)]
+pub struct NewUserForm {
+    pub first_name: String,
+    pub last_name:  String,
+    pub email:      String,
+    pub password:   String,
+}
+pub async fn signup_form(payload: &mut Multipart) -> NewUserForm {
+    let mut form: NewUserForm = NewUserForm {
+        first_name: "".to_string(),
+        last_name:  "".to_string(),
+        email:      "".to_string(),
+        password:   "".to_string(),
+    };
+
+    while let Some(item) = payload.next().await {
+        let mut field: Field = item.expect("split_payload err");
+        while let Some(chunk) = field.next().await {
+            let data = chunk.expect("split_payload err chunk");
+            if let Ok(s) = std::str::from_utf8(&data) {
+                let data_string = s.to_string();
+                if field.name() == "first_name" {
+                    form.first_name = data_string
+                }
+                else if field.name() == "last_name" {
+                    form.last_name = data_string
+                }
+                else if field.name() == "email" {
+                    form.email = data_string
+                }
+                else if field.name() == "password" {
+                    form.password = data_string
+                }
+            }
+        }
+    }
+    form
+}
+pub async fn signup(session: Session, mut payload: Multipart) -> actix_web::Result<HttpResponse> {
     if is_signed_in(&session) {
         return crate::views::not_found_page(session).await;
     }
-    let r_data = LoginUser {
-        email:    data.email.clone(),
-        password: data.password.clone(),
-    };
+    let form = signup_form(payload.borrow_mut()).await;
     let l_data = NewUser2 {
-        first_name: data.first_name.clone(),
-        last_name:  data.last_name.clone(),
-        email:      data.email.clone(),
-        password:   data.password.clone(),
+        first_name: form.first_name.clone(),
+        last_name:  form.last_name.clone(),
+        email:      form.email.clone(),
+        password:   form.password.clone(),
     }; 
     let res = request_post::<NewUser2, AuthResp> (
         URL.to_owned() + &"/signup/".to_string(),
