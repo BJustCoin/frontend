cb03842b6438fd4d5ffc76592628c01fc594e980
interesnij <ochkarik1983@mail.ru>
2024-11-17 23:28:08 +0300

    J

--- src/utils/reqwest.rs
+++ src/utils/reqwest.rs
@@ -45,17 +45,17 @@ where
     let allow_body = method == reqwest::Method::POST || method == reqwest::Method::PUT;
     let mut req = reqwest::Client::new()
         .request(method, url)
-        .header("Content-Type", "application/json");
+        //.header("Content-Type", "application/json");
 
     //if is_auth { 
     //    if let Some(token) = get_token(){
     //        req = req.bearer_auth(token);
-    //    }
+     //   }
     //}
 
-    //if allow_body { 
-    //    req = req.json(body);
-    //}
+    if allow_body { 
+        req = req.json(body);
+    }
     println!("=============");
     println!("Req: {:?}", req);
     let res_resp = req.send().await;
