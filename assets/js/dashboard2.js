7875446223123d4ed9aee9d46f7598c373147220
interesnij <ochkarik1983@mail.ru>
2024-11-27 15:08:35 +0300

    j

--- assets/js/dashboard2.js
+++ assets/js/dashboard2.js
@@ -128,14 +128,14 @@ var chart = AmCharts.makeChart( "chartdiv21", {
     "categoryField": "date",
     // EVENTS
     "stockEvents": [ {
-      "date": new Date( 2020, 8, 19 ),
+      "date": new Date( 2024, 8, 19 ),
       "type": "sign",
       "backgroundColor": "#fc00ff",
       "graph": "g1",
       "text": "S",
       "description": "This is description of an event"
     }, {
-      "date": new Date( 2020, 10, 19 ),
+      "date": new Date( 2024, 10, 19 ),
       "type": "flag",
       "backgroundColor": "#FFFFFF",
       "backgroundAlpha": 0.5,
@@ -143,7 +143,7 @@ var chart = AmCharts.makeChart( "chartdiv21", {
       "text": "F",
       "description": "Some longer\ntext can also\n be added"
     }, {
-      "date": new Date( 2020, 11, 10 ),
+      "date": new Date( 2024, 11, 10 ),
       "showOnAxis": true,
       "backgroundColor": "#85CDE6",
       "type": "pin",
@@ -151,7 +151,7 @@ var chart = AmCharts.makeChart( "chartdiv21", {
       "graph": "g1",
       "description": "This is description of an event"
     }, {
-      "date": new Date( 2020, 11, 26 ),
+      "date": new Date( 2024, 11, 26 ),
       "showOnAxis": true,
       "backgroundColor": "#85CDE6",
       "type": "pin",
@@ -159,44 +159,44 @@ var chart = AmCharts.makeChart( "chartdiv21", {
       "graph": "g1",
       "description": "This is description of an event"
     }, {
-      "date": new Date( 2021, 0, 3 ),
+      "date": new Date( 2023, 0, 3 ),
       "type": "sign",
       "backgroundColor": "#85CDE6",
       "graph": "g1",
       "text": "U",
       "description": "This is description of an event"
     }, {
-      "date": new Date( 2021, 1, 6 ),
+      "date": new Date( 2023, 1, 6 ),
       "type": "sign",
       "graph": "g1",
       "text": "D",
       "description": "This is description of an event"
     }, {
-      "date": new Date( 2021, 3, 5 ),
+      "date": new Date( 2024, 3, 5 ),
       "type": "sign",
       "graph": "g1",
       "text": "L",
       "description": "This is description of an event"
     }, {
-      "date": new Date( 2021, 3, 5 ),
+      "date": new Date( 2024, 3, 5 ),
       "type": "sign",
       "graph": "g1",
       "text": "R",
       "description": "This is description of an event"
     }, {
-      "date": new Date( 2021, 5, 15 ),
+      "date": new Date( 2024, 5, 15 ),
       "type": "arrowUp",
       "backgroundColor": "#00CC00",
       "graph": "g1",
       "description": "This is description of an event"
     }, {
-      "date": new Date( 2021, 6, 25 ),
+      "date": new Date( 2024, 6, 25 ),
       "type": "arrowDown",
       "backgroundColor": "#CC0000",
       "graph": "g1",
       "description": "This is description of an event"
     }, {
-      "date": new Date( 2021, 8, 1 ),
+      "date": new Date( 2024, 8, 1 ),
       "type": "text",
       "graph": "g1",
       "text": "Longer text can\nalso be displayed",
@@ -268,132 +268,132 @@ var chart = AmCharts.makeChart("chartdiv4", {
     "autoMarginOffset":20,
     "marginRight":20,
     "dataProvider": [{
-        "date": "2021-10-01",
+        "date": "2024-10-01",
         "value": 3,
         "fromValue": 2,
         "toValue": 5
     }, {
-        "date": "2021-10-02",
+        "date": "2024-10-02",
         "value": 5,
         "fromValue": 4,
         "toValue": 6
     }, {
-        "date": "2021-10-03",
+        "date": "2024-10-03",
         "value": 15,
         "fromValue": 12,
         "toValue": 18
     }, {
-        "date": "2021-10-04",
+        "date": "2024-10-04",
         "value": 13,
         "fromValue": 10.4,
         "toValue": 15.6
     }, {
-        "date": "2021-10-05",
+        "date": "2024-10-05",
         "value": 17,
         "fromValue": 13.6,
         "toValue": 20.4
     }, {
-        "date": "2021-10-06",
+        "date": "2024-10-06",
         "value": 15,
         "fromValue": 12,
         "toValue": 18
     }, {
-        "date": "2021-10-09",
+        "date": "2024-10-09",
         "value": 19,
         "fromValue": 15.2,
         "toValue": 22.8
     }, {
-        "date": "2021-10-10",
+        "date": "2024-10-10",
         "value": 21,
         "fromValue": 16.8,
         "toValue": 25.2
     }, {
-        "date": "2021-10-11",
+        "date": "2024-10-11",
         "value": 20,
         "fromValue": 16,
         "toValue": 24
     }, {
-        "date": "2021-10-12",
+        "date": "2024-10-12",
         "value": 20,
         "fromValue": 16,
         "toValue": 24
     }, {
-        "date": "2021-10-13",
+        "date": "2024-10-13",
         "value": 19,
         "fromValue": 15.2,
         "toValue": 22.8
     }, {
-        "date": "2021-10-16",
+        "date": "2024-10-16",
         "value": 25,
         "fromValue": 20,
         "toValue": 30
     }, {
-        "date": "2021-10-17",
+        "date": "2024-10-17",
         "value": 24,
         "fromValue": 19.2,
         "toValue": 28.8
     }, {
-        "date": "2021-10-18",
+        "date": "2024-10-18",
         "value": 26,
         "fromValue": 20.8,
         "toValue": 31.2
     }, {
-        "date": "2021-10-19",
+        "date": "2024-10-19",
         "value": 27,
         "fromValue": 21.6,
         "toValue": 32.4
     }, {
-        "date": "2021-10-20",
+        "date": "2024-10-20",
         "value": 25,
         "fromValue": 20,
         "toValue": 30
     }, {
-        "date": "2021-10-23",
+        "date": "2024-10-23",
         "value": 29,
         "fromValue": 23.2,
         "toValue": 34.8
     }, {
-        "date": "2021-10-24",
+        "date": "2024-10-24",
         "value": 28,
         "fromValue": 22.4,
         "toValue": 33.6
     }, {
-        "date": "2021-10-25",
+        "date": "2024-10-25",
         "value": 30,
         "fromValue": 24,
         "toValue": 36
     }, {
-        "date": "2021-10-26",
+        "date": "2024-10-26",
         "value": 72,
         "fromValue": 57.6,
         "toValue": 86.4
     }, {
-        "date": "2021-10-27",
+        "date": "2024-10-27",
         "value": 43,
         "fromValue": 34.4,
         "toValue": 51.6
     }, {
-        "date": "2021-10-30",
+        "date": "2024-10-30",
         "value": 31,
         "fromValue": 24.8,
         "toValue": 37.2
     }, {
-        "date": "2021-11-01",
+        "date": "2024-11-01",
         "value": 30,
         "fromValue": 24,
         "toValue": 36
     }, {
-        "date": "2021-11-02",
+        "date": "2024-11-02",
         "value": 29,
         "fromValue": 23.2,
         "toValue": 34.8
     }, {
-        "date": "2021-11-03",
+        "date": "2024-11-03",
         "value": 27,
         "fromValue": 21.6,
         "toValue": 32.4
     }, {
-        "date": "2021-11-04",
+        "date": "2024-11-04",
         "value": 26,
         "fromValue": 20.8,
         "toValue": 31.2
@@ -474,7 +474,7 @@ var chart = AmCharts.makeChart("chartdiv4", {
 chart.addListener("dataUpdated", zoomChart);
 
 function zoomChart(){
-    chart.zoomToDates(new Date(2021,9,20, 15), new Date(2021,10,3,12));
+    chart.zoomToDates(new Date(2024,9,20, 15), new Date(2024,10,3,12));
 }
 
 $(function () {
