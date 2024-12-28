169806cdf36982b3a9f491388dcb6b04e6a598f3
interesnij <ochkarik1983@mail.ru>
2024-12-28 13:23:11 +0300

    J

--- assets/js/auth3.js
+++ assets/js/auth3.js
@@ -292,6 +292,6 @@ on('body', 'click', '#send_token', function() {
 
 on('body', 'input', '#id_email', function() {
     send_token = document.body.querySelector("#send_token").parentElement;
-    send_token.parentElement.classList.remove("hide");
+    send_token.classList.remove("hide");
     document.body.querySelector("#id_token").parentElement.parentElement.classList.remove("hide");
 });   
