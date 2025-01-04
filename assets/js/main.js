6343d638de9cef595e2902b88160401933d39b52
interesnij <ochkarik1983@mail.ru>
2025-01-04 12:01:23 +0300

    H

---
+++ assets/js/main.js
@@ -0,0 +1,31 @@
+function elementInViewport(el){var bounds = el.getBoundingClientRect();return ((bounds.top + bounds.height > 0) && (window.innerHeight - bounds.top > 0));}
+
+function scrolled(_block) {
+    try {
+        box = _block.querySelector('.next_page_list');
+        if (box && box.classList.contains("next_page_list")) {
+            inViewport = elementInViewport(box);
+            if (inViewport) {
+              box.classList.remove("next_page_list");
+              paginate(box);
+            }
+        };
+    } catch {return}
+};
+function paginate(block) {
+    var link_3 = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP');
+    link_3.open('GET', location.protocol + "//" + location.host + block.getAttribute("data-link") + "&ajax=2", true);
+    link_3.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+
+    link_3.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            var elem = document.createElement('span');
+            elem.innerHTML = link_3.responseText;
+            block.parentElement.insertAdjacentHTML('beforeend', elem.querySelector(".is_paginate").innerHTML)
+            block.remove()
+        }
+    }
+    link_3.send();
+};
+
+scrolled(document.body.querySelector(".container-full"));
