b9c6a43e25db2fbeff1ac7e68184e8b31deafef8
interesnij <ochkarik1983@mail.ru>
2025-01-10 17:42:33 +0300

    J

--- assets/js/connect12.js
+++ assets/js/connect12.js
@@ -206,13 +206,13 @@ window.addEventListener('load', function () {
                     console.log("buy_bjustcoin");
                     type = this.getAttribute("data-type");
                     amount = this.parentElement.querySelector(".number_of_tokens").value;
-                    //a = 10 ** 18;
-                    //final_value = amount * a;
+                    a = 10 ** 18;
+                    final_value = amount * a;
                     console.log("ico stage", type);
                     if (type == 1) { 
                         contract.methods.buyICOToken().send({
                             from: defaultAccount,
-                            value: amount,
+                            value: final_value,
                         });
                     }
                     else if (type == 2) {
