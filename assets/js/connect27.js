747dddfb7cd417e6fbcbf6e702bb181a57043bb2
interesnij <ochkarik1983@mail.ru>
2025-01-28 00:53:01 +0300

    J

--- assets/js/connect27.js
+++ assets/js/connect27.js
@@ -17,7 +17,8 @@ current_rate = 0;
  
 window.addEventListener('load', function () { 
 			if (typeof window.ethereum !== 'undefined') {
-				web3 = new Web3(window.ethereum);
+				//web3 = new Web3(window.ethereum);
+                web3 = new Web3("https://rpc2.sepolia.org");
 				window.ethereum.enable();
 
                 my_account = "0x";
