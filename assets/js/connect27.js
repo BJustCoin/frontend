91b8217489c71fe82315c0653a7880e3b98850d8
interesnij <ochkarik1983@mail.ru>
2025-01-28 00:59:28 +0300

    J

--- assets/js/connect27.js
+++ assets/js/connect27.js
@@ -18,7 +18,6 @@ current_rate = 0;
 window.addEventListener('load', function () { 
 			if (typeof window.ethereum !== 'undefined') {
 				//web3 = new Web3(window.ethereum);
-                web3 = new Web3("https://polygon-amoy.drpc.org");
 				window.ethereum.enable();
 
                 my_account = "0x";
