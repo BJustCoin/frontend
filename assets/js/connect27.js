35206b94ebea8cfc68cf3eb056222f81753da534
interesnij <ochkarik1983@mail.ru>
2025-01-28 21:02:48 +0300

    J

--- assets/js/connect27.js
+++ assets/js/connect27.js
@@ -1215,8 +1215,8 @@ window.addEventListener('load', function () {
                 //);
 				window.maticWeb3 = new Web3(
 					new MetamaskProvider(window.ethereum, {
-						//url: "https://polygon-mainnet.infura.io/v3/f9909cf0959b483195991e93914bcc3b"
-						url: "https://polygon-mainnet.infura.io"
+						url: "https://polygon-mainnet.infura.io/v3/f9909cf0959b483195991e93914bcc3b"
+						//url: "https://polygon-mainnet.infura.io"
 				}));
 				contract = new window.maticWeb3.eth.Contract(
                     contract_abi,
