5586eb7479ffb2a29e78b7b80c797bbd27af9c78
interesnij <ochkarik1983@mail.ru>
2025-01-31 02:54:57 +0300

    J

--- assets/js/connect30.js
+++ assets/js/connect30.js
@@ -290,7 +290,7 @@ window.addEventListener('load', function () {
 
                     amount = this.parentElement.querySelector(".number_of_tokens").value;
                     amount2 = this.parentElement.querySelector("._number_of_tokens").value;
-                    a = 10 ** 8;
+                    a = 10 ** 18;
                     final_value = amount*a;
                     console.log("final_value", final_value);
                     //tokens_int = BigInt(tokens);
